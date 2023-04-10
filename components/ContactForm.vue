863ab60577f35bbb772e4fb4015027435e85cc9e
Kevin Delahunt <120161030+mus-tard@users.noreply.github.com>
2023-04-10 16:36:13 -0400

    Added form AJAX

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,3 +1,22 @@
+<script setup>
+
+const handleSubmit = (event) => {
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+
+</script>
+
 <template>
             <div class="deets">
             <p><span class="bold">Contact</span> - 514-714-6090</p>
@@ -6,7 +25,7 @@
         </div>
     <div class="container">
 
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" @submit.prevent="handleSubmit" data-netlify="true">
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
             </p>
