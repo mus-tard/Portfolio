0d3ce551c52ced3277ad52c1f6e978ed014e66ce
Charles K. Delahunt <90341103+dela0130@users.noreply.github.com>
2023-04-25 11:28:36 -0400

    brought in contact deets from form component

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,6 +7,11 @@
 <template>
         <div class="container">
             <!-- <pre>{{ story }}</pre> -->
+            <div class="deets">
+              <p><span class="bold">Contact</span> - 514-714-6090</p>
+              <p><span class="bold">Email</span> - contact@kevd.design</p>
+
+          </div>
             <StoryblokComponent 
                 :blok="story.content" 
                 v-if="story"
